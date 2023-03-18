d97e545fe291aaf166cbae057fa854715414fe1b
developer-bot-hub <117781817+developer-bot-hub@users.noreply.github.com>
2023-03-17 23:14:29 -0300

    rb
    rb

---
+++ interfaz.rb
@@ -0,0 +1,32 @@
+interfaz_pepita = %w(
+  energia
+  cantar!
+  comer_lombriz!
+  volar_en_circulos!
+)
+  
+interfaz_norita = %w(
+  cantar!
+  comer_lombriz!
+  volar_en_circulos!
+)
+  
+interfaz_mercedes = %w(
+  cantar!
+)
+# ¿Qué interfaz comparten Mercedes y Norita?
+interfaz_compartida_entre_mercedes_y_norita = %w(
+  cantar!
+)
+
+# ¿Qué interfaz comparten Pepita y Norita?
+interfaz_compartida_entre_pepita_y_norita = %w(
+  cantar!
+  comer_lombriz!
+  volar_en_circulos!
+)
+
+# ¿Qué interfaz comparten Mercedes, Norita y Pepita?
+interfaz_compartida_entre_todas = %w(
+  cantar! 
+)
